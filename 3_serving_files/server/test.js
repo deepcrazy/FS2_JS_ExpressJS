51ee8d4f1fb64e1e2a395e29d30a99c4704f6a23
deepcrazy <312.gupta@gmail.com>
2019-10-27 13:58:25 -0400

    Added validation code for username entered and simple comments added in js file and polishing of html file also

--- 3_serving_files/server/test.js
+++ 3_serving_files/server/test.js
@@ -4,10 +4,7 @@ try {
     const port = 1080;
     const url = require('url');
 
-    // appServer.use(express.logger());
-    // appServer.set("view options", {layout: false});
     // appServer.use(express.static(__dirname + '/public'));
-
     // appServer.set('view engine', 'jade');    //  worked
 
     appServer.engine('html', require('ejs').renderFile);
@@ -18,8 +15,6 @@ try {
     });
 
     appServer.get('/:id', (req,res) => {
-        // console.log("coming home..!!");
-        // console.log(__dirname);
         if (req.params.id == 'home'){
             res.sendFile(__dirname + "/public/home.html");
         }
@@ -29,21 +24,25 @@ try {
         if (req.params.id == 'contact'){
             res.sendFile(__dirname + "/public/contactUs.html");
         }
-        
-
         // res.render(__dirname + '/public/index');     //worked
         // res.render(__dirname + '/views/home.html');
     });
 
     appServer.get('/login/:loginDetails', (req, res) => {
-        console.log("User login details: " + req.params.loginDetails);
-        // res.send("Reached node server file..!!");
-        res.send(req.params.loginDetails);
+        //  Validation: The  length of name entered should be greater than 4
+        if (req.params.loginDetails.split("=")[1].length > 4) {
+            console.log("User login details: " + req.params.loginDetails.split("=")[1]);
+            res.send("You have entered name: " + req.params.loginDetails.split("=")[1]);
+        }
+        else {
+            console.log("Entered name length is less than 4")
+            res.send("Please enter name of length more than 4..!!");
+        }
     })
 
     appServer.listen(port, () => {
         console.log("App started  on port: " + port);
     })
 } catch (error) {
-    console.log("Error occured: " + error);
+    console.log("Error occurred: " + error);        //  Error Handling
 }
